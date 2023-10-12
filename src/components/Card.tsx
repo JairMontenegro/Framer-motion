7a2b2a995f84e6b7f42a8d6efd8bda1b15ac43e5
jair <jairmontenegro@quadi.co>
2023-10-12 11:32:09 -0500

    feat: adding motion to div card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import {motion} from "framer-motion"
+
 
 interface Props {
   srcset: string;
@@ -9,13 +11,19 @@ interface Props {
 
 const Card: React.FC<Props> = ({ srcset, src , alt, cardName }) => {
   return (
-    <div>
+    <motion.div className="card"
+    whileHover={{
+      scale:1.2,
+      transition:{duration: .3},
+      
+    }}
+    >
       <picture>
       <p>{cardName}</p>
         <source media="(max-width: 375px)" srcSet={srcset}/>
         <img src={src} alt={alt} />
       </picture>
-    </div>
+    </motion.div>
   );
 };
 
